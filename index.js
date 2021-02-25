c35d6140f490e8bfae84e80a238fd2896eed974c
Lucca Sanwald <lucca@flowkey.com>
2021-02-25 16:02:22 +0100

    add initial files to test it

---
+++ index.js
@@ -0,0 +1,30 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const axios = require('axios');
+
+const GHOST_INSPECTOR_API_KEY = process.env.GHOST_INSPECTOR_API_KEY
+
+async function getTestResults(suiteID,startURL){
+    const response = await axios.get(`https://api.ghostinspector.com/v1/suites/${GHOST_INSPECTOR_SUITE_ID}/execute/?apiKey=${GHOST_INSPECTOR_API_KEY}&startUrl=${START_URL}`)
+    console.log(response);
+    console.log(response.data);
+    // TODO: return only test results
+    return {
+        allTestsSuccessful: true
+    }
+}
+
+try {
+    const startURL = core.getInput('startURL');
+    const suiteID = core.getInput('suiteID');
+
+    const testResults = getTestResults(startURL,suiteID);
+
+    if(testResults.allTestsSuccessful){
+        core.setOutput("All tests successful");
+    }else {
+        core.setFailed("At least one test failed");
+    }
+} catch (error) {
+    core.setFailed(error.message);
+}
