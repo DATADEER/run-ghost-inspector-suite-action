c3784def226528caa170f0f2d62467e640cec67b
Lucca Sanwald <lucca@flowkey.com>
2021-02-25 17:43:36 +0100

    wrap big ass async function around everything

--- index.js
+++ index.js
@@ -2,33 +2,35 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const axios = require('axios');
 
-const GHOST_INSPECTOR_API_KEY = core.getInput('GHOST_INSPECTOR_API_KEY');
+(async function(){
+    const GHOST_INSPECTOR_API_KEY = core.getInput('GHOST_INSPECTOR_API_KEY');
 
-async function getSuiteResult(suiteID,startURL){
-    const response = await axios.get(`https://api.ghostinspector.com/v1/suites/${suiteID}/execute/?apiKey=${GHOST_INSPECTOR_API_KEY}&startUrl=${startURL}`)
-    console.log("REQUEST RESPONSE", response);
-    console.log("RESULTS", response.data.data);
-    return response.data.data;
-}
-
-function areAllTestsSuccessful(suiteResult){
-    const allTestResults = suiteResult.map((test) => {
-        return test.passing && test.screenshotComparePassing
-    })
-    return !allTestResults.includes(false)
-}
-
-try {
-    const startURL = core.getInput('startURL');
-    const suiteID = core.getInput('suiteID');
-
-    const suiteResult = await getSuiteResult(suiteID,startURL);
-
-    if(areAllTestsSuccessful(suiteResult)){
-        core.setOutput("SUCCESS","All tests successful");
-    }else {
-        core.setFailed(`At least one test failed ${suiteResult}`);
+    async function getSuiteResult(suiteID,startURL){
+        const response = await axios.get(`https://api.ghostinspector.com/v1/suites/${suiteID}/execute/?apiKey=${GHOST_INSPECTOR_API_KEY}&startUrl=${startURL}`)
+        console.log("REQUEST RESPONSE", response);
+        console.log("RESULTS", response.data.data);
+        return response.data.data;
     }
-} catch (error) {
-    core.setFailed(error.message);
-}
+
+    function areAllTestsSuccessful(suiteResult){
+        const allTestResults = suiteResult.map((test) => {
+            return test.passing && test.screenshotComparePassing
+        })
+        return !allTestResults.includes(false)
+    }
+
+    try {
+        const startURL = core.getInput('startURL');
+        const suiteID = core.getInput('suiteID');
+
+        const suiteResult = await getSuiteResult(suiteID,startURL);
+
+        if(areAllTestsSuccessful(suiteResult)){
+            core.setOutput("SUCCESS","All tests successful");
+        }else {
+            core.setFailed(`At least one test failed ${suiteResult}`);
+        }
+    } catch (error) {
+        core.setFailed(error.message);
+    }
+}())
