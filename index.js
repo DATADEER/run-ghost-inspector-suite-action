41abfab5e388aabe9d5e89c82ceb675a5fa09a1d
Lucca Sanwald <lucca@flowkey.com>
2021-02-25 17:09:46 +0100

    get GHOST_INSPECTOR_API_KEY as input finish logic regarding basic test checking

--- index.js
+++ index.js
@@ -2,28 +2,32 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const axios = require('axios');
 
-const GHOST_INSPECTOR_API_KEY = process.env.GHOST_INSPECTOR_API_KEY;
+const GHOST_INSPECTOR_API_KEY = core.getInput('GHOST_INSPECTOR_API_KEY');
 
-async function getTestResults(suiteID,startURL){
+async function getSuiteResult(suiteID,startURL){
     const response = await axios.get(`https://api.ghostinspector.com/v1/suites/${suiteID}/execute/?apiKey=${GHOST_INSPECTOR_API_KEY}&startUrl=${startURL}`)
-    console.log(response);
-    console.log(response.data);
-    // TODO: return only test results
-    return {
-        allTestsSuccessful: true
-    }
+    console.log("REQUEST RESPONSE", response);
+    console.log("RESULTS", response.data.data);
+    return response.data.data;
+}
+
+function areAllTestsSuccessful(suiteResult){
+    const allTestResults = suiteResult.map((test) => {
+        return test.passing && test.screenshotComparePassing
+    })
+    return !allTestResults.includes(false)
 }
 
 try {
     const startURL = core.getInput('startURL');
     const suiteID = core.getInput('suiteID');
 
-    const testResults = getTestResults(startURL,suiteID);
+    const suiteResult = getSuiteResult(suiteID,startURL);
 
-    if(testResults.allTestsSuccessful){
-        core.setOutput("All tests successful");
+    if(areAllTestsSuccessful(suiteResult)){
+        core.setOutput("SUCCESS","All tests successful");
     }else {
-        core.setFailed("At least one test failed");
+        core.setFailed(`At least one test failed ${suiteResult}`);
     }
 } catch (error) {
     core.setFailed(error.message);
